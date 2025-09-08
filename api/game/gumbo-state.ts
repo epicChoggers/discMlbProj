1c59241bc3b32d4992568383156771047e243587
BartonBowell <carson@carsonppowell.com>
2025-09-08 16:19:36 -0700

    gumbo

--- api/game/gumbo-state.ts
+++ api/game/gumbo-state.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { gumboGameDataService } from '../lib/gumboGameDataService'
+import { gumboGameDataService } from '../lib/gumboGameDataService.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Set CORS headers
